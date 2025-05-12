e82d2d9d8ed93c89695e8e98759683d7539fc33b
nijowww <jigballeta@gmail.com>
2025-05-13 00:52:32 +0800

    Added Mobile Joystick, Jump Button

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,5 +1,8 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 
 public class InputManager : MonoBehaviour
@@ -9,6 +12,11 @@ public class InputManager : MonoBehaviour
 
     private PlayerMotor motor;
     private PlayerLook look;
+
+    // FOR MOBILE
+    public Joystick moveJoystick;
+    public Button jumpButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -17,19 +25,45 @@ public class InputManager : MonoBehaviour
         motor = GetComponent<PlayerMotor>();
         look = GetComponent<PlayerLook>();
         onFoot.Jump.performed += ctx => motor.Jump();
+        if (jumpButton != null)
+        {
+            jumpButton.onClick.AddListener(() => motor.Jump());
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //tell the playermotor to move using the value from our movement action.
-        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        Vector2 moveInput = onFoot.Movement.ReadValue<Vector2>();
+
+        // Add joystick input (if assigned)
+        if (moveJoystick != null)
+        {
+            moveInput += new Vector2(moveJoystick.Horizontal, moveJoystick.Vertical);
+        }
+
+        motor.ProcessMove(moveInput);
     }
 
-    private void LateUpdate()
+    void LateUpdate()
     {
-        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+        Vector2 lookInput = onFoot.Look.ReadValue<Vector2>();
+
+#if UNITY_ANDROID || UNITY_IOS
+    if (Input.touchCount > 0)
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Moved && !IsPointerOverUIObject(touch))
+        {
+            lookInput += touch.deltaPosition * 0.1f; // Tune sensitivity
+        }
     }
+#endif
+
+        look.ProcessLook(lookInput);
+    }
+
 
     private void OnEnable()
     {
@@ -39,5 +73,14 @@ public class InputManager : MonoBehaviour
     private void OnDisable()
     {
         onFoot.Disable();
+    }
+
+    private bool IsPointerOverUIObject(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = touch.position;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
     }
 }
