792ae7d214967e10cd5198b7327ef55262692355
nijowww <jigballeta@gmail.com>
2025-05-26 11:27:07 +0800

    DayNightCycle
    Still has an issue won't restart the day

--- Assets/Scripts/Interactables/MainPowerSwitch.cs
+++ Assets/Scripts/Interactables/MainPowerSwitch.cs
@@ -1,15 +1,26 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainPowerSwitch : Interactable
 {
     [SerializeField] private MonitorController[] monitors;
+    [SerializeField] private DayNightCycle dayNightCycle; 
 
     protected override void Interact()
     {
         Debug.Log("Switch Interacted");
+
         foreach (MonitorController monitor in monitors)
         {
             monitor.PowerOn();
+        }
+
+        if (dayNightCycle != null)
+        {
+            dayNightCycle.StartDay(); 
+        }
+        else
+        {
+            Debug.LogWarning("DayNightCycle reference not assigned!");
         }
     }
 
