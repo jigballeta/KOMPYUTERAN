325d6f0567cb421fb3a3407bbb7b2645415470b6
Juan Eduardo Batalla <stelajed@gmail.com>
2025-06-11 21:16:10 +0800

    Complete Main Menu
    fix some ui ingame
    complete menu

--- Assets/Scripts/UI Script/Script/MenuScene.cs
+++ Assets/Scripts/UI Script/Script/MenuScene.cs
@@ -1,32 +1,91 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScene : MonoBehaviour
 {
     private CanvasGroup fadegroup;
     private float fadeinspeed = 0.33f;
 
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    [Header("Toggles (Optional)")]
+    public Toggle musicToggle;
+    public Toggle soundToggle;
+
     private void Start()
     {
-        // Grab the only CanvasGroup in the scene
         fadegroup = FindAnyObjectByType<CanvasGroup>();
-
-        // Start with a white screen;
         fadegroup.alpha = 1;
+
+        ShowMainMenu();
+
+        // Load previous toggle states (optional)
+        if (musicToggle != null)
+            musicToggle.isOn = PlayerPrefs.GetInt("MusicEnabled", 1) == 1;
+
+        if (soundToggle != null)
+            soundToggle.isOn = PlayerPrefs.GetInt("SoundEnabled", 1) == 1;
     }
 
     private void Update()
     {
-        // Fade in
         fadegroup.alpha = 1 - Time.timeSinceLevelLoad * fadeinspeed;
     }
 
-    // Buttons
+    // Called by Play button
     public void OnPlayClick()
     {
-        Debug.Log("Play button has been clicked!");
+        Debug.Log("Play button clicked!");
+        SceneManager.LoadScene("SampleScene"); // Change to your actual scene name
+    }
 
-        // Load the gameplay scene (replace "GameScene" with your actual scene name)
-        SceneManager.LoadScene("SampleScene");
+    // Called by Settings button
+    public void OnSettingsClick()
+    {
+        Debug.Log("Settings button clicked");
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // Called by Back button in settings
+    public void OnBackClick()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // Called by Quit button
+    public void OnQuitClick()
+    {
+        Debug.Log("Quit button clicked");
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // Called when music toggle is changed
+    public void OnMusicToggle(bool isOn)
+    {
+        Debug.Log("Music Toggle: " + isOn);
+        PlayerPrefs.SetInt("MusicEnabled", isOn ? 1 : 0);
+        // TODO: Add your music on/off logic here
+    }
+
+    // Called when sound toggle is changed
+    public void OnSoundToggle(bool isOn)
+    {
+        Debug.Log("Sound Toggle: " + isOn);
+        PlayerPrefs.SetInt("SoundEnabled", isOn ? 1 : 0);
+        // TODO: Add your sound on/off logic here
+    }
+
+    private void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
     }
 }
---
+++ Assets/Scripts/UI Script/Script/Settings.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+    public Toggle musicToggle;
+    public Slider soundSlider;
+    public Button helpButton;
+    public GameObject helpPanel;
+
+    public AudioSource musicSource;
+    public AudioMixer audioMixer;
+
+    void Start()
+    {
+        // Load music setting
+        bool isMusicOn = PlayerPrefs.GetInt("MusicOn", 1) == 1;
+        if (musicToggle != null)
+            musicToggle.isOn = true;
+        else
+            Debug.LogWarning("Music Toggle not assigned in Inspector!");
+        musicSource.mute = !isMusicOn;
+
+        // Load sound volume
+        float savedVolume = PlayerPrefs.GetFloat("SoundVolume", 0.75f);
+        soundSlider.value = savedVolume;
+        SetSoundVolume(savedVolume);
+
+
+    }
+
+    public void OnMusicToggle(bool isOn)
+    {
+        musicSource.mute = !isOn;
+        PlayerPrefs.SetInt("MusicOn", isOn ? 1 : 0);
+    }
+
+    public void SetSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SoundVolume", volume);
+    }
+
+    public void ToggleHelpPanel()
+    {
+        helpPanel.SetActive(!helpPanel.activeSelf);
+    }
+}
